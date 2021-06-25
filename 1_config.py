corrected bug in the calculation of the scores

1_config.py
@@ -1,8 +1,8 @@
 ######################################
-animal = '211'
+animal = '111'
 max_impedance = 2500000
-select_channels = False
-delete_circus_folder = True
+select_channels = True
+delete_circus_folder = False
 alert_when_done = True
 ######################################
 
@@ -25,6 +25,7 @@ theta_band = [4, 12]
 spike_band = [300, 3000]
 sampling_rate = 20000
 
+start_time = time.time()
 # folder preparation
 animal_folder = r'E:/anxiety_ephys/' + animal + '/'
 sub_folder = 'circus'
@@ -134,14 +135,15 @@ if select_channels:
             mPFC_channels.remove(discarded_channel)
         elif discarded_channel in vHIP_channels:
             vHIP_channels.remove(discarded_channel)
-    np.save(target_folder + 'discarded_channels', discarded_channels)
+  #  np.save(target_folder + 'discarded_channels', discarded_channels.astype(np.uint16))
+
 mPFC_channels = list(np.array(mPFC_channels)[np.array(mPFC_impedance) < max_impedance]) #remove channels above max impedance
 vHIP_channels = list(np.array(vHIP_channels)[np.array(vHIP_impedance) < max_impedance])
 # pads[x] denotes the pad corresponding to data['amplifier_data'][x]
 mPFC_pads = [pta.index(channel) + 1 for channel in mPFC_channels]
 vHIP_pads = [pta.index(channel) + 1 for channel in vHIP_channels]
-np.save(target_folder + 'utils/vHIP_pads', vHIP_pads)#save the pads corresponding to the channels/data_rows
-np.save(target_folder + 'utils/mPFC_pads', mPFC_pads)
+np.save(target_folder + 'utils/vHIP_pads', np.array(vHIP_pads, dtype=np.uint16))#save the pads corresponding to the channels/data_rows
+np.save(target_folder + 'utils/mPFC_pads', np.array(mPFC_pads, dtype=np.uint16))
 # probe file 'graph'
 graph = []  #needs to be empty for legacy reasons
 
@@ -156,13 +158,15 @@ for i in range(len(mPFC_pads)):
 total_nb_channels = len(mPFC_channels)
 channels = list(np.arange(0, total_nb_channels))
 
-mouse_is_late = {'2021-02-19_mBWfus010_EZM_ephys': 70,
-                 '2021-02-19_mBWfus009_EZM_ephys': 42,
+mouse_is_late = {'2021-02-19_mBWfus010_EZM_ephys': 70, #keys: all the mice that are not present in the maze at the video trigger
+                 '2021-02-19_mBWfus009_EZM_ephys': 42, #values: seconds after the trigger until they are present
                  '2021-02-26_mBWfus012_EZM_ephys': 35}
 
 accomodation = 20 #time after mouse is introduced into the maze to cut off in seconds (only used if mouse_is_late)
+#prepare the data files for cropping.py:
 for index, experiment_name in tqdm(enumerate(experiment_names)):
-    physio_trigger = physio_triggers[experiment_name]
+    physio_trigger = physio_triggers[experiment_name] #20000Hz trigger
+    #calculate offset for late mice:
     if experiment_name in mouse_is_late:
         off = (mouse_is_late[experiment_name] + accomodation)*sampling_rate
     else:
@@ -171,34 +175,35 @@ for index, experiment_name in tqdm(enumerate(experiment_names)):
 
     rhd_folder = animal_folder + experiment_name + '/ephys/'
     rhd_files = natsorted(os.listdir(rhd_folder))
-    amp_channels = read_data(rhd_folder + rhd_files[0])['amplifier_channels']  # time: 02:28min for 25 files
-    channel_list = [int(channel['native_channel_name'][2:]) for channel in amp_channels]
-    mPFC_indices = [channel_list.index(valid_channel) for valid_channel in mPFC_channels]
+    amp_channels = read_data(rhd_folder + rhd_files[0])['amplifier_channels']  # all amplifier channels
+    channel_list = [int(channel['native_channel_name'][2:]) for channel in amp_channels] # all amplifier channel names
+    mPFC_indices = [channel_list.index(valid_channel) for valid_channel in mPFC_channels] #the indices of the valid channels
     vHIP_indices = [channel_list.index(valid_channel) for valid_channel in vHIP_channels]
     total_size = 0
     for i, rhdfile in enumerate(rhd_files):
-        rhd_data = np.array(read_data(rhd_folder + rhdfile)['amplifier_data'], dtype=np.int16)
-        mPFC_data = rhd_data[mPFC_indices]
+        rhd_data = np.array(read_data(rhd_folder + rhdfile)['amplifier_data'], dtype=np.int16)#rows are unordered channls
+        mPFC_data = rhd_data[mPFC_indices] #data ordered by pad, only valid channels
         vHIP_data = rhd_data[vHIP_indices]
         if i == 0:
+            #make empty np array to store the data of all rhd files of the session
             mPFC_concatenated = np.zeros((mPFC_data.shape[0], mPFC_data.shape[1] * len(rhd_files)), dtype=np.int16)
             vHIP_concatenated = np.zeros((vHIP_data.shape[0], vHIP_data.shape[1] * len(rhd_files)), dtype=np.int16)
-        mPFC_concatenated[:, total_size:total_size + mPFC_data.shape[1]] = mPFC_data
+        mPFC_concatenated[:, total_size:total_size + mPFC_data.shape[1]] = mPFC_data #assign data from the current rhd
+        #file to its timeslot
         vHIP_concatenated[:, total_size:total_size + vHIP_data.shape[1]] = vHIP_data
         total_size += vHIP_data.shape[1]
-    mPFC_concatenated = mPFC_concatenated[:, physio_trigger:total_size]
+    mPFC_concatenated = mPFC_concatenated[:, physio_trigger:total_size] #crop the data to the relevant time-window
     vHIP_concatenated = vHIP_concatenated[:, physio_trigger:total_size]
-    np.save(target_folder + 'mPFC_raw/' + experiment_names[index], mPFC_concatenated)
-    np.save(target_folder + 'vHIP_raw/' + experiment_names[index], vHIP_concatenated)
+    np.save(target_folder + 'mPFC_raw/' + experiment_names[index], mPFC_concatenated.astype(np.int16))
+    np.save(target_folder + 'vHIP_raw/' + experiment_names[index], vHIP_concatenated.astype(np.int16))
+    #filter mPFC data to spike-range
     sos_spike = butter(N=butter_order, Wn=spike_band, btype='bandpass', analog=False, output='sos', fs=sampling_rate)
     spike_filtered = sosfiltfilt(sos_spike, mPFC_concatenated - np.median(mPFC_concatenated, axis=0)[None, :], axis=1)
-    np.save(target_folder + 'mPFC_spike_range/' + experiment_names[index], spike_filtered)#use this data to make cutout timestamps
+    np.save(target_folder + 'mPFC_spike_range/' + experiment_names[index], spike_filtered.astype(np.int16))
+    #use this data to make cutout timestamps
 
-
-
-
-# prepare probe.prb file
-radius = 100
+radius = 100 #radius in muekrometers to consider for clustering
+#prepare data that will be on top of the templat in the probe file
 cap1 = ('total_nb_channels = ' + str(total_nb_channels)
         + '\nradius = ' + str(radius)
         + '\ngraph = ' + str(graph)
@@ -213,7 +218,7 @@ with open(templates + 'probespecs.prb', 'r') as f:
 with open(target_folder + 'utils/probe.prb', 'a') as f:
     f.write(t)
 
-# prepare parameters.params file
+#prepare data that will be on top of the templat in the parameters file
 cap2 = ('[data]'
         + '\nnb_channels = ' + str(total_nb_channels)
         + '\nmapping = ' + target_folder + 'utils/probe.prb'
@@ -227,6 +232,8 @@ with open(templates + 'parameters.params', 'r') as f:
 with open(param_file, 'a') as f:
     f.write(t)
 
-print('Config for animal {} done!'.format(animal))
+end_time = time.time()
+print('Config for animal {} done! \nTime needed: {} minutes.'.format(animal, (end_time-start_time)/60))
+
 if alert_when_done:
     utils.alert()

2_cropping.py
@@ -1,5 +1,5 @@
 ######################################
-animal = '211'
+animal = '111'
 alert_when_done = False
 ######################################
 
@@ -24,6 +24,7 @@ spike_band = [300, 3000]
 sampling_rate = 20000
 frame_rate = 50
 
+start_time = time.time()
 # folder preparation
 animal_folder = r'E:/anxiety_ephys/' + animal + '/'
 sub_folder = 'circus'
@@ -36,7 +37,6 @@ templates = r'E:/pipe/spykingcircus_sorter_2/templates/'
 circus_entrypoint = target_folder + 'dat_files/' + experiment_names[0] + '_0.dat'
 if os.listdir(target_folder + 'dat_files_mod'):
     raise Exception('Croppig already done, to redo run config again first.')
-#os.mkdir(target_folder + 'movement_files')#todo remove
 
 mouse_is_late = {'2021-02-19_mBWfus010_EZM_ephys': 70,
                  '2021-02-19_mBWfus009_EZM_ephys': 42,
@@ -60,7 +60,7 @@ for index, experiment_name in enumerate(experiment_names):
     movement_trigger = int(float(data[point - 2:point + 3]) * frame_rate)
 
     if experiment_name in mouse_is_late:
-        off = (mouse_is_late[experiment_name] + accomodation) * sampling_rate
+        off = (mouse_is_late[experiment_name] + accomodation) * frame_rate
     else:
         off = 0
     movement_trigger += off
@@ -80,9 +80,9 @@ for index, experiment_name in enumerate(experiment_names):
         vHIP_concatenated = vHIP_concatenated[:, boolean_sampling_rate]
         mPFC_spike_range = mPFC_spike_range[:, boolean_sampling_rate]
         xy = xy[:, boolean_frame_rate]
-        np.save(target_folder + 'mPFC_spike_range/' + experiment_name, mPFC_spike_range)
+        np.save(target_folder + 'mPFC_spike_range/' + experiment_name, mPFC_spike_range.astype(np.int16))
 
-    np.save(target_folder + 'movement_files/' + experiment_name, xy)
+    np.save(target_folder + 'movement_files/' + experiment_name, xy.astype(np.float32))
 
     sos_theta = butter(N=butter_order, Wn=theta_band, btype='bandpass', analog=False, output='sos', fs=sampling_rate)
     theta_filtered = sosfiltfilt(sos_theta, vHIP_concatenated, axis=1)
@@ -90,9 +90,9 @@ for index, experiment_name in enumerate(experiment_names):
     data_for_spikesorting = np.transpose(mPFC_concatenated)
     # save files:
     data_for_spikesorting.tofile(target_folder + 'dat_files/' + experiment_names[index] + '_' + str(index) + '.dat')
-    np.save(target_folder + 'vHIP_phase/' + experiment_name, hilbert_phase)
+    np.save(target_folder + 'vHIP_phase/' + experiment_name, hilbert_phase.astype(np.int16)) #todo determine dtype
     logbook[index] = data_for_spikesorting.shape[0]
-np.save(target_folder + 'utils/logbook', logbook)
+np.save(target_folder + 'utils/logbook', logbook.astype(np.uint32))
 
 
 
@@ -102,12 +102,6 @@ np.save(target_folder + 'utils/logbook', logbook)
 cluster_command = 'spyking-circus ' + circus_entrypoint + ' -c 10'
 os.system(cluster_command)
 
-# args = shlex.split(cluster_command)
-# cluster = subprocess.run(args, stdout=subprocess.PIPE,
-#                          encoding='ascii')
-# print(cluster.stdout)
-#
-# print('clustering return code: ', cluster.returncode)
 
 
 converter_command = 'spyking-circus ' + circus_entrypoint + ' -m converting -c 10'
@@ -116,21 +110,21 @@ args = shlex.split(converter_command)
 converter = subprocess.run(args, stdout=subprocess.PIPE,
                            input='a\n', encoding='ascii')
 
-print(converter.returncode)
+print('Converter return_code: {}'.format(converter.returncode))
 # print(converter.stdout)
-
+end_time = time.time()
+print('Cropping for animal {} done! \nTime needed: {} minutes.'.format(animal, (end_time-start_time)/60))
+if alert_when_done:
+    alert()
 
 # start viewer
 viewer_command = '@echo off \ncall conda activate circus \ncircus-gui-python ' + circus_entrypoint
 with open(target_folder + 'utils/start_viewer.bat', 'w') as f:
     f.write(viewer_command)
+viewer_command = 'circus-gui-python ' + circus_entrypoint
 args = shlex.split(viewer_command)
-if alert_when_done:
-    alert()
+
 viewer = subprocess.run(args, stdout=subprocess.PIPE,
                         encoding='ascii')
 
-print(viewer.returncode)
-# print(viewer.stdout)
-
-print('Cropping for animal {} done!'.format(animal))
+print('Viewer return_code: {}'.format(viewer.returncode))

3_post_processing.py
@@ -1,5 +1,5 @@
 ######################################
-animal = '211'
+animal = '111'
 alert_when_done = True
 ######################################
 
@@ -11,9 +11,12 @@ from tqdm import tqdm
 import os
 from natsort import natsorted
 import utils
+import time
 
 
-
+start_time = time.time()
+frame_rate = 50
+sampling_rate = 20000
 animal_folder = r'E:/anxiety_ephys/' + animal + '/'
 experiment_names = natsorted(os.listdir(animal_folder))
 if 'circus' in experiment_names:
@@ -28,7 +31,7 @@ sclusters = 'spike_clusters.npy'
 infofile = 'cluster_info.tsv'
 
 
-number_of_bins_transitions = 20  # in 10 second window around transitions
+number_of_bins_transitions = 20  # in 5 second window around transitions
 number_of_bins_phase = 8  # phaseplot
 number_of_bins_isi = 200
 
@@ -50,23 +53,23 @@ logbook_3 = np.zeros(len(experiment_names)+1, dtype=np.int64)
 for i in range(logbook_3.size):
     logbook_3[i] = np.sum(logbook_2[0:i+1])
 original_logbook_3 = logbook_3
-logbook_3 = logbook_3*50//20000
+logbook_3 = logbook_3*frame_rate//sampling_rate
 
 
 
 original_spiketimes = np.load(folder + stimes)
-spiketimes = original_spiketimes*50//20000  #sampled at 50Hz
+spiketimes = original_spiketimes*frame_rate//sampling_rate  #sampled at 50Hz
 clusters = np.load(folder + sclusters)
 data = np.zeros((int(indexer.max()+1), logbook_3[-1]), dtype=np.uint8)
 original_data = np.zeros((int(indexer.max()+1), original_logbook_3[-1]), dtype=bool)
 
 
 
-for index, time in enumerate(spiketimes):
-    data[indexer[clusters[index]],time] += 1
+for index, timepoint in enumerate(spiketimes):
+    data[indexer[clusters[index]],timepoint] += 1
 
-for index, time in enumerate(original_spiketimes):
-    original_data[indexer[clusters[index]], time] += 1
+for index, timepoint in enumerate(original_spiketimes):
+    original_data[indexer[clusters[index]], timepoint] += 1
 
 
 
@@ -74,7 +77,7 @@ cluster_names = np.zeros(data.shape[0], dtype = np.uint8)
 for i in range(1,cluster_names.size):
     cluster_names[i] =  np.where(indexer == i)[0]
 cluster_names[0] = 255
-np.save(target_folder + 'utils/cluster_names', cluster_names)
+np.save(target_folder + 'utils/cluster_names', cluster_names.astype(np.uint16))
 
 
 # lw =0.4
@@ -89,8 +92,8 @@ np.save(target_folder + 'utils/cluster_names', cluster_names)
 
 
 for i in range(logbook_3.size-1):
-    np.save(target_folder + 'spikes_50/' + experiment_names[i], data[:,logbook_3[i]:logbook_3[i+1]])
-    np.save(target_folder + 'spikes_20000/' + experiment_names[i], original_data[:,original_logbook_3[i]:original_logbook_3[i+1]])
+    np.save(target_folder + 'spikes_50/' + experiment_names[i], data[:,logbook_3[i]:logbook_3[i+1]].astype(np.uint8))
+    np.save(target_folder + 'spikes_20000/' + experiment_names[i], original_data[:,original_logbook_3[i]:original_logbook_3[i+1]].astype(bool))
 
 vHIP_pads = np.load(target_folder + 'utils/vHIP_pads.npy')
 archive = utils.create_archive(vHIP_pads, cluster_names, number_of_bins_transitions, number_of_bins_phase, number_of_bins_isi)
@@ -100,9 +103,9 @@ archive.loc[:, ('characteristics', 'overall_firing_rate')] = info.loc[:, 'fr']
 archive.loc[:, ('characteristics', 'purity')] = info.loc[:, 'purity']
 archive.loc[:, ('characteristics', 'data_row')] = info.loc[:, 'ch']
 
-
-
 archive.to_pickle(target_folder + 'archive.pkl')
-print('post processing for animal {} done!'.format(animal))
+
+end_time = time.time()
+print('Post_processing for animal {} done! \nTime needed: {} minutes'.format(animal, (end_time-start_time)/60))
 if alert_when_done:
     utils.alert()

4_sanity_check.py
@@ -1,7 +1,7 @@
 #%%
 
 ######################################
-animal = '211'
+animal = '111'
 alert_when_done = True
 ######################################
 
@@ -17,9 +17,11 @@ from time import sleep
 from shutil import rmtree
 from scipy.stats import pearsonr
 from scipy.ndimage.filters import gaussian_filter1d
+import time
 
 
 
+start_time = time.time()
 animal_folder = r'E:/anxiety_ephys/' + animal + '/'
 experiment_names = natsorted(os.listdir(animal_folder))
 if 'circus' in experiment_names:
@@ -54,7 +56,7 @@ for experiment_name in experiment_names:
     experiment_plots = plot_folder+ experiment_name
     os.mkdir(experiment_plots)
     spikes_20000 = np.load(target_folder + 'spikes_20000/' + experiment_name + '.npy')
-    mPFC_spike_range = np.load(target_folder + 'mPFC_spike_range/' + experiment_name + '.npy').astype(np.int16)#todo
+    mPFC_spike_range = np.load(target_folder + 'mPFC_spike_range/' + experiment_name + '.npy')
     number_of_units = spikes_20000.shape[0]
     number_of_channels = mPFC_spike_range.shape[0]
 
@@ -122,15 +124,15 @@ for unit in range(number_of_units):
     plt.close(fig)
 #%%
 sigma=3
-pearson_coefficients = pd.DataFrame(index= sorted_units[:][2], columns=natsorted(waveform_dict.keys()))
-p_values = pd.DataFrame(index= sorted_units[:][2], columns=natsorted(waveform_dict.keys()))
-
+pearson_coefficients = pd.DataFrame(index= np.array(sorted_units)[:,2], columns=natsorted(waveform_dict.keys()), dtype=np.float32)
+p_values = pd.DataFrame(index= np.array(sorted_units)[:,2], columns=natsorted(waveform_dict.keys()), dtype=np.float32)
+#%%
 fig, axs = plt.subplots(number_of_units, len(isi_dict.keys()), sharex=True)
 fig.set_figheight(25)
 fig.set_figwidth(25)
 for unit in range(number_of_units):
     unit_id = sorted_units[unit][2]
-    unit_index = sorted_units[unit][1]
+    unit_index = int(sorted_units[unit][1])
     channel = sorted_units[unit][0]
     sum = np.zeros(number_of_bins_isi)
     for index, key in enumerate(natsorted(isi_dict.keys())):
@@ -146,24 +148,24 @@ for unit in range(number_of_units):
 
     for index, key in enumerate(natsorted(waveform_dict.keys())):
         session_isi = isi_dict[key][unit_index, :]
-        pearson_coefficients.loc[unit_id, key], p_values.loc[unit_id, key] =\
-            pearsonr(gaussian_filter1d(mean, sigma=sigma), gaussian_filter1d(session_isi,sigma=sigma))
-pearson_coefficients.to_pickle(target_folder + 'pearson_coefficients_isi.pkl')
-p_values.to_pickle(target_folder + 'p_values_isi.pkl')
+        pearson_coefficients.loc[unit_id, key], p_values.loc[unit_id, key] \
+            = pearsonr(gaussian_filter1d(mean, sigma=sigma), gaussian_filter1d(session_isi,sigma=sigma))
+pearson_coefficients.to_pickle(target_folder + 'utils/pearson_coefficients_isi.pkl')
+p_values.to_pickle(target_folder + 'utils/p_values_isi.pkl')
 
-plt.savefig(sanity_plots + 'isi_unit_' + str(unit_id))
+plt.savefig(sanity_plots + 'isi_all_units')
 plt.show()
 plt.close(fig)
 
 #%%
-pearson_coefficients = pd.DataFrame(index= sorted_units[:][2], columns=natsorted(waveform_dict.keys()))
-p_values = pd.DataFrame(index= sorted_units[:][2], columns=natsorted(waveform_dict.keys()))
-fig, axs = plt.subplots(1, number_of_units, sharex=True, sharey=True)
+pearson_coefficients = pd.DataFrame(index= np.array(sorted_units)[:,2], columns=natsorted(waveform_dict.keys()), dtype=np.float32)
+p_values = pd.DataFrame(index= np.array(sorted_units)[:,2], columns=natsorted(waveform_dict.keys()), dtype=np.float32)
+fig, axs = plt.subplots(1, number_of_units-1, sharex=True, sharey=True)
 fig.set_figheight(25)
 fig.set_figwidth(25)
-for unit in range(number_of_units):
+for unit in range(1,number_of_units):
     unit_id = sorted_units[unit][2]
-    unit_index = sorted_units[unit][1]
+    unit_index = int(sorted_units[unit][1])
     channel = sorted_units[unit][0]
     sum = np.zeros(60)
     for index, key in enumerate(natsorted(waveform_dict.keys())):
@@ -172,25 +174,27 @@ for unit in range(number_of_units):
         else:
             sum = 1
         toplot = waveform_dict[key][unit_index,:,:] + np.linspace(0, 100*number_of_channels, number_of_channels)[:, None]
-        axs[unit].plot((toplot + index * 15).T, 'b', linewidth=0.5)
+        axs[unit-1].plot((toplot + index * 15).T, 'b', linewidth=0.5)
     mean = sum / len(waveform_dict.keys())
-    axs[unit].set_yticks(np.linspace(0, 100*number_of_channels, number_of_channels))
-    axs[unit].set_yticklabels(mPFC_pads)
-    axs[unit].set_xticks(np.linspace(0,60, 2))
-    axs[unit].set_xticklabels([-1,2])
-    axs[unit].set_title(unit_id, loc='right')
+    axs[unit-1].set_yticks(np.linspace(0, 100*number_of_channels, number_of_channels))
+    axs[unit-1].set_yticklabels(mPFC_pads)
+    axs[unit-1].set_xticks(np.linspace(0,60, 2))
+    axs[unit-1].set_xticklabels([-1,2])
+    axs[unit-1].set_title(unit_id, loc='right')
     archive.loc[unit_id, idx['mean_waveform',:]] = mean
     for index, key in enumerate(natsorted(waveform_dict.keys())):
-        main_waveform = waveform_dict[key][unit_index, int[channel], :]
+        main_waveform = waveform_dict[key][unit_index, int(channel), :]
         pearson_coefficients.loc[unit_id, key], p_values.loc[unit_id, key] = pearsonr(mean, main_waveform)
-pearson_coefficients.to_pickle(target_folder + 'pearson_coefficients_waveform.pkl')
-p_values.to_pickle(target_folder + 'p_values_waveform.pkl')
+pearson_coefficients.to_pickle(target_folder + 'utils/pearson_coefficients_waveform.pkl')
+p_values.to_pickle(target_folder + 'utils/p_values_waveform.pkl')
 
 plt.savefig(sanity_plots + 'waveform_all_units')
 plt.show()
 plt.close(fig)
 archive.to_pickle(target_folder + 'archive.pkl')
-
+end_time = time.time()
+print('Sanity_check for animal {} done! \nTime needed: {} minutes'.format(animal, (end_time-start_time)/60))
 if alert_when_done:
     alert()
+
 

6_treatment_analysis.py
@@ -1,29 +1,33 @@
 ######################################
-animals = ['11'] # set of animals, 2 char string, example animal 9: '09'
-treatment = 'medication' # one of ['none', 'saline', 'medication']
-score = 'treatment_score'  # one of:         ['ezm_closed_score', 'ezm_transition_score', 'of_corners_score', 'of_middle_score', 'treatment_score']
-threshold = 20                      # recommended: [0                 ,  0                    ,  0                ,  0            , 20   ]
+animals = ['09', '11', '12'] # set of animals, 2 char string, example animal 9: '09'
+treatment = 'none' # one of ['none', 'saline', 'medication']
+score = 'ezm_transition_score'  # one of:         ['ezm_closed_score', 'ezm_transition_score', 'of_corners_score', 'of_middle_score', 'treatment_score',
+#'ezm_closed_score_2', 'ezm_transition_score_2', 'of_corners_score_2', 'of_middle_score_2' ]
+threshold = 0.2                 # recommended: [0.2                 ,  0.2                    ,  0.2                ,  0.2            , 20   ]
 data_separations = ['under_threshold_all', 'over_threshold_all', 'under_threshold_plus', 'over_threshold_plus',
-                   'under_threshold_minus', 'over_threshold_minus']  # selection of ['under_threshold_all', 'over_threshold_all', 'under_threshold_plus', 'over_threshold_plus',
-# 'under_threshold_minus', 'over_threshold_minus']
+                   'under_threshold_minus', 'over_threshold_minus', 'all', 'all_minus', 'all_plus']  # selection of ['under_threshold_all', 'over_threshold_all', 'under_threshold_plus', 'over_threshold_plus',
+# 'under_threshold_minus', 'over_threshold_minus', 'all', 'all_minus', 'all_plus']
 
 #significance plus/minus: plus['ezm_closed_score': firing rate higer in closed area,
 # 'ezm_transition_score': firing rate higher in transition zones, 'of_corners_score': firing rate higher in corners,
 # 'of_middle_score': firing rate higher in the middle, 'treatment_score': firing rate higher before treatment]
 
-toplot = ['phase']
+toplot = ['circle', 'grid', 'arms', 'corners', 'transitions', 'phase']
 # selection of: ['circle', 'grid', 'arms', 'corners', 'transitions', 'phase']
-transition_modes = ['open_closed_entrytime']
+transition_modes = ['open_closed_entrytime', 'open_closed_exittime', 'closed_open_entrytime', 'closed_open_exittime',
+'lingering_entrytime', 'lingering_exittime', 'prolonged_open_closed_entrytime', 'prolonged_open_closed_exittime',
+'prolonged_closed_open_entrytime', 'prolonged_closed_open_exittime', 'withdraw_entrytime', 'withdraw_exittime',
+'nosedip_starttime', 'nosedip_stoptime']
 # selection of: ['open_closed_entrytime', 'open_closed_exittime', 'closed_open_entrytime', 'closed_open_exittime',
 # 'lingering_entrytime', 'lingering_exittime', 'prolonged_open_closed_entrytime', 'prolonged_open_closed_exittime',
 # 'prolonged_closed_open_entrytime', 'prolonged_closed_open_exittime', 'withdraw_entrytime', 'withdraw_exittime',
 # 'nosedip_starttime', 'nosedip_stoptime']
 phase_modes = ['theta_phase_OFT', 'theta_phase_EZM', 'theta_phase_before', 'theta_phase_after']
 # selection of: ['theta_phase_OFT', 'theta_phase_EZM', 'theta_phase_before', 'theta_phase_after']
-delete_plot_folder = False
-show = True
-save = False
-alert_when_done = True
+delete_plot_folder = True
+show = False
+save = True
+alert_when_done = False
 ######################################
 import copy
 import pandas as pd
@@ -35,6 +39,7 @@ import pickle5 as pkl
 import treatment_plots
 from utils import alert
 
+start_time = time.time()
 treatment_dict = {'none': '1', 'saline': '2', 'medication': '3'}
 _ , animals = (list(t) for t in zip(*sorted(zip([int(animal) for animal in animals], animals))))
 days = [treatment_dict[treatment] + animal for animal in animals]
@@ -51,7 +56,7 @@ if not os.path.exists(threshold_folder):
     os.mkdir(threshold_folder)
 if os.path.exists(plot_folder):
     if delete_plot_folder:
-        shutil.rmtree(plot_folder, ignore_errors=True)
+        shutil.rmtree(plot_folder)
         time.sleep(3)
         os.mkdir(plot_folder)
 else:
@@ -63,22 +68,27 @@ archives = []
 for day in days:
     target_folder = data_folder + day + '/' + sorter + '/'
     archive = pd.read_pickle(target_folder + 'archive.pkl')
-#    archive_index = archive.index.tolist()
-    archive.drop(255, axis='index')
- #   archive.index = [day + '_' + str(old_index) for old_index in archive_index]
+    archive.drop(index=255, axis='index', inplace=True)
+    archive_index = archive.index.tolist()
+    archive.index = [day + '_' + str(old_index) for old_index in archive_index]
     archives.append(archive)
-archive = pd.concat(archives, ignore_index=True, join= 'inner')
-plusminus = score[:-6]
-# scorer = archive.loc[:,('characteristics', score)]
-# n1 = archive.loc[:,('characteristics', score)].values < threshold
-# n2 = archive.index != -1
+archive = pd.concat(archives, join= 'inner')
 
-if plusminus == 'treatment':
+
+
+
+if score == 'treatment_score':
+    plusminus = score[:-6]
     mean_before = archive.loc[:, ('characteristics', 'mean_before')].values
     mean_after = archive.loc[:, ('characteristics', 'mean_after')].values
     plus_minus_values = (mean_before-mean_after)*100/mean_before
     score_values = np.abs(plus_minus_values)
+elif score[-1] == '2':
+    plusminus = score[:-8]
+    score_values = np.abs(archive.loc[:, ('characteristics', plusminus)].values)
+    plus_minus_values = archive.loc[:, ('characteristics', plusminus)].values
 else:
+    plusminus = score[:-6]
     score_values = archive.loc[:, ('characteristics', score)].values
     plus_minus_values = archive.loc[:,('characteristics',plusminus)].values
 
@@ -92,6 +102,12 @@ over_threshold_plus = archive.loc[
     np.logical_and(score_values >= threshold, plus_minus_values > 0)]
 over_threshold_minus = archive.loc[
     np.logical_and(score_values >= threshold, plus_minus_values <= 0)]
+all =  archive
+all_plus =  archive.loc[plus_minus_values > 0]
+all_minus = archive.loc[plus_minus_values <= 0]
+with open(plot_folder + 'distribution.txt', 'a') as f:
+    f.write('all units:' + ': len: ' + str(len(archive.index.tolist())) + '\nunits: ' +
+            str(archive.index.tolist()) + '\n')
 
 for data_separation in data_separations:
     if data_separation == 'under_threshold_all':
@@ -106,8 +122,21 @@ for data_separation in data_separations:
         data = under_threshold_minus
     elif data_separation == 'over_threshold_minus':
         data = over_threshold_minus
+
+    elif data_separation == 'all':
+        data = all
+    elif data_separation == 'all_plus':
+        data = all_plus
+    elif data_separation == 'all_minus':
+        data = all_minus
+
     else:
         raise Exception('invalid data separation entry: ' + data_separation)
+
+    with open(plot_folder + 'distribution.txt', 'a') as f:
+        f.write(data_separation + ': len: ' + str(len(data.index.tolist())) + '\nunits: ' +
+        str(data.index.tolist()) + '\n')
+
     if 'circle' in toplot:
         treatment_plots.plot_circle(plot_folder, data.loc[:, 'ROI_EZM'], data_separation, show=show, save=save)
     if 'grid' in toplot:
@@ -140,7 +169,8 @@ for data_separation in data_separations:
                 treatment_plots.plot_phase_all_pads(plot_folder, data.loc[:, pad_columns], pad_columns, data_separation, phase_mode,
                                                     show=show, save=save)
 
-
-print('treatment_analysis done for animals: {}, treatment: {}, score: {}, threshold: {}'.format(animals, treatment, score, threshold))
+end_time = time.time()
+print('Treatment_analysis done for animals: {}, treatment: {}, score: {}, threshold: {}\nTime needed: {} minutes'
+      .format(animals, treatment, score, threshold,(end_time-start_time)/60))
 if alert_when_done:
     alert()

day_plots.py
@@ -49,7 +49,10 @@ def get_ezm_ROI_masks(n, transition_size):
 def plot_trace(environment, plot_folder, experiment_name, aligned, cluster_names, single_figures, multi_figure,
                sigma=10, minp=0, maxp=95, n=150, show=False, save=True, filter=False):
     mode = 'trace_filter_' + str(filter)
-    file_name = plot_folder + experiment_name + '_' + mode + '_sigma' + str(sigma) + '_n' + str(n) + '_minp' + str(
+    file_name = plot_folder + experiment_name + '_' + mode
+    if filter:
+        file_name += '_sigma' + str(sigma)
+    file_name +=  '_n' + str(n) + '_minp' + str(
         minp) + '_maxp' + str(maxp) + '_'
     content = np.copy(aligned)
     if filter:
@@ -362,7 +365,7 @@ def plot_events(plot_folder, experiment_name, aligned, cluster_names, mode, even
     transition_indices = [transition_index for transition_index in event_indices if
                           transition_index + n <= content.shape[1] and transition_index - n >= 0]
     if not transition_indices:  # check if there are any valid events
-        return
+        return archive
     binned = np.zeros((number_of_units, number_of_bins, len(transition_indices)),
                       dtype=np.float32)  # unit, bar, transition_index
     for tindex, transition_index in enumerate(transition_indices):
@@ -642,13 +645,14 @@ def plot_corners(plot_folder, experiment_name, aligned, cluster_names, archive, 
 
 # computes the EZM score
 def get_ezm_score(rois):
-    mean = np.mean(rois, axis=1)
-    mean = np.where(mean != 0, mean, 1)
-    rois = (rois - mean[:, None]) * 100 / mean[:, None]
+    # mean = np.mean(rois, axis=1)
+    # mean = np.where(mean != 0, mean, 1)
+    # rois = (rois - mean[:, None]) * 100 / mean[:, None]
     a1 = 0.25 * (np.abs(rois[:, 5] - rois[:, 4]) + np.abs(rois[:, 5] - rois[:, 6]) + np.abs(
-        rois[:, 7] - rois[:, 7]) + np.abs(rois[:, 7] - rois[:, 6]))
+        rois[:, 7] - rois[:, 4]) + np.abs(rois[:, 7] - rois[:, 6]))
     b1 = 0.5 * (np.abs(rois[:, 5] - rois[:, 7]) + np.abs(rois[:, 4] - rois[:, 6]))
     open_close = (a1 - b1) / (a1 + b1)
+    closed = (rois[:, 5] + rois[:, 7]) / 2 - (rois[:, 4] + rois[:, 6]) / 2
 
     a2 = 1 / 16 * (np.abs(rois[:, 0] - rois[:, 4]) + np.abs(rois[:, 0] - rois[:, 7])
                    + np.abs(rois[:, 0] - rois[:, 6]) + np.abs(rois[:, 0] - rois[:, 5])
@@ -665,16 +669,15 @@ def get_ezm_score(rois):
                    + np.abs(rois[:, 4] - rois[:, 5]) + np.abs(rois[:, 5] - rois[:, 7])
                    + np.abs(rois[:, 5] - rois[:, 6]) + np.abs(rois[:, 6] - rois[:, 7]))
     crossing = (a2 - b2) / (a2 + b2)
-    closed = (rois[:, 5] + rois[:, 7]) / 2
-    transition = (rois[:, 0] + rois[:, 1] + rois[:, 2] + rois[:, 3]) / 4
+    transition = (rois[:, 0] + rois[:, 1] + rois[:, 2] + rois[:, 3]) / 4 - (rois[:, 5] + rois[:, 7] + rois[:, 4] + rois[:, 6]) / 4
     return open_close, crossing, closed, transition
 
 
 # computes the OF score
 def get_of_score(rois):
-    mean = np.mean(rois, axis=1)
-    mean = np.where(mean != 0, mean, 1)
-    rois = (rois - mean[:, None]) * 100 / mean[:, None]
+    # mean = np.mean(rois, axis=1)
+    # mean = np.where(mean != 0, mean, 1)
+    # rois = (rois - mean[:, None]) * 100 / mean[:, None]
 
     a1 = 1 / 20 * (np.abs(rois[:, 0] - rois[:, 4]) + np.abs(rois[:, 0] - rois[:, 5])
                    + np.abs(rois[:, 0] - rois[:, 6]) + np.abs(rois[:, 0] - rois[:, 7])
@@ -697,7 +700,7 @@ def get_of_score(rois):
                    + np.abs(rois[:, 6] - rois[:, 8]))
 
     of_corners_score = (a1 - b1) / (a1 + b1)
-    of_corners = (rois[:, 0] + rois[:, 1] + rois[:, 2] + rois[:, 3]) / 4
+    of_corners = (rois[:, 0] + rois[:, 1] + rois[:, 2] + rois[:, 3]) / 4 - (rois[:, 4] + rois[:, 5] + rois[:, 6] + rois[:, 7] + rois[:, 8]) / 5
     a2 = 1 / 8 * (np.abs(rois[:, 8] - rois[:, 4]) + np.abs(rois[:, 8] - rois[:, 5])
                   + np.abs(rois[:, 8] - rois[:, 6]) + np.abs(rois[:, 8] - rois[:, 7])
                   + np.abs(rois[:, 0] - rois[:, 8]) + np.abs(rois[:, 1] - rois[:, 8])
@@ -719,7 +722,7 @@ def get_of_score(rois):
                    + np.abs(rois[:, 5] - rois[:, 7]) + np.abs(rois[:, 6] - rois[:, 7]))
 
     of_middle_score = (a2 - b2) / (a2 + b2)
-    of_middle = rois[:, 8]
+    of_middle = rois[:, 8] - (rois[:, 0] + rois[:, 1] + rois[:, 2] + rois[:, 3] + rois[:, 4] + rois[:, 5] + rois[:, 6] + rois[:, 7]) / 8
     return of_corners_score, of_middle_score, of_corners, of_middle
 
 

utils.py
@@ -87,5 +87,5 @@ def create_archive(vHIP_pads, cluster_names, number_of_bins_transitions, number_
               + phase_ranges
     tuples = list(zip(level_1, level_2))
     columns = pd.MultiIndex.from_tuples(tuples)
-    archive = pd.DataFrame(index=cluster_names, columns=columns)
+    archive = pd.DataFrame(index=cluster_names, columns=columns, dtype=np.float32)
     return archive